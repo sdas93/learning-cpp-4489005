806fd6e82f5037d161fa0c24cebaf8743f25e4cb
sdas93 <sdmanofsteel@gmail.com>
2025-05-03 12:05:52 +0000

    added code

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Enter your name:" <<std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Good Afternoon!" << str;
 
     std::cout << std::endl << std::endl;
     return (0);
