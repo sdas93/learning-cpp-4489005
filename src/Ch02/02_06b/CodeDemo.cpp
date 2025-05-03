46be7d6565f723c60f2aff94734d7da4f403d61f
sdas93 <sdmanofsteel@gmail.com>
2025-05-03 14:14:23 +0000

    added code

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,20 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cstdint>
+
+#define CAPACITY 5000
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[About to perform the addition!]" <<std::endl;
+#endif
+
+    large += small;
+    std::cout << "The large number is:" << large << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
